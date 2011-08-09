b652572b78c89792f8e42adcdc84554a2f02bd0e
coderx0x <coderx0x@gmail.com>
2011-08-09 11:12:19 +0700

    first commit

---
+++ src/config.hpp
@@ -0,0 +1,7 @@
+#ifndef SGS_CONFIG_HPP_
+#define SGS_CONFIG_HPP_
+
+#define SGS_VERSION 0.0.1
+
+
+#endif //SGS_CONFIG_HPP_
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "config.hpp"
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Server Game System: v" << SGS_VERSION << std::endl;
+    return 0;
+}
